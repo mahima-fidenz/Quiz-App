329b0c8ef4f0d9d0faf17e573f5039a8c08dcfd2
Mahima de Silva <mahima.d@fidenz.com>
2024-07-23 14:43:47 +0530

    Fetch past quizzes from firestore.

--- lib/providers/firebase_provider.dart
+++ lib/providers/firebase_provider.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart'
     hide EmailAuthProvider, PhoneAuthProvider;
@@ -13,6 +15,10 @@ class FirebaseProvider extends ChangeNotifier {
   bool _loggedIn = false;
   bool get loggedIn => _loggedIn;
 
+  StreamSubscription<QuerySnapshot>? _pastQuizzesSubscription;
+  List<Quiz> _pastQuizzes = [];
+  List<Quiz> get pastQuizzes => _pastQuizzes;
+
   Future<void> init() async {
     FirebaseUIAuth.configureProviders([
       EmailAuthProvider(),
@@ -21,8 +27,25 @@ class FirebaseProvider extends ChangeNotifier {
     FirebaseAuth.instance.userChanges().listen((user) {
       if (user != null) {
         _loggedIn = true;
+        _pastQuizzesSubscription = FirebaseFirestore.instance
+            .collection('quizzes')
+            .where('userId', isEqualTo: user.uid)
+            .orderBy('timestamp', descending: true)
+            .snapshots()
+            .listen((snapshot) {
+          _pastQuizzes = [];
+          for (final document in snapshot.docs) {
+            _pastQuizzes.add(Quiz(
+                timestamp: document.data()['timestamp'] as int,
+                finalscore: document.data()['finalscore'] as int,
+                fullscore: document.data()['fullscore'] as int));
+          }
+          notifyListeners();
+        });
       } else {
         _loggedIn = false;
+        _pastQuizzes = [];
+        _pastQuizzesSubscription?.cancel();
       }
       notifyListeners();
     });
--- lib/screens/past_quizzes_screen.dart
+++ lib/screens/past_quizzes_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:provider/provider.dart';
+import 'package:quiz_app/providers/firebase_provider.dart';
 
 class PastQuizzesScreen extends StatelessWidget {
   const PastQuizzesScreen({super.key});
@@ -6,19 +9,61 @@ class PastQuizzesScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          'Past Quizzes',
-          style: Theme.of(context).textTheme.displayLarge,
-        ),
-      ),
-      body: const SizedBox.expand(
-        child: Center(
-          child: Placeholder(
-            color: Colors.red,
+        appBar: AppBar(
+          title: Text(
+            'Past Quizzes',
+            style: Theme.of(context).textTheme.displayLarge,
           ),
         ),
-      ),
-    );
+        body: Consumer<FirebaseProvider>(
+          builder: (context, firebaseProvider, _) {
+            if (firebaseProvider.pastQuizzes.isEmpty) {
+              return const Center(
+                child: Text(
+                  'No quizzes available',
+                  style: TextStyle(
+                    fontSize: 18,
+                    color: Colors.grey,
+                  ),
+                ),
+              );
+            }
+
+            return ListView.builder(
+              itemCount: firebaseProvider.pastQuizzes.length,
+              itemBuilder: (context, index) {
+                var quiz = firebaseProvider.pastQuizzes[index];
+                return Card(
+                  margin:
+                      const EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(15),
+                  ),
+                  elevation: 4,
+                  child: ListTile(
+                      contentPadding: const EdgeInsets.all(16),
+                      leading: Icon(
+                        Icons.quiz,
+                        size: 40,
+                        color: Theme.of(context).primaryColor,
+                      ),
+                      title: Text(
+                        'Quiz on ${DateFormat('yMMMMd HH:mm:ss').format((DateTime.fromMicrosecondsSinceEpoch(quiz.timestamp)))}',
+                        style: Theme.of(context).textTheme.bodyLarge?.copyWith(
+                              fontWeight: FontWeight.bold,
+                              color: Colors.black87,
+                            ),
+                      ),
+                      subtitle: Text(
+                        'Score: ${quiz.finalscore}/${quiz.fullscore}',
+                        style: Theme.of(context).textTheme.bodyMedium?.copyWith(
+                              color: Colors.black54,
+                            ),
+                      )),
+                );
+              },
+            );
+          },
+        ));
   }
 }
